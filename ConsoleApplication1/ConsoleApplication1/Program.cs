6c1baa144fcc04570f77e0acea30de94b19884e3
fei396 <fei396@163.com>
2016-04-17 22:55:54 +0800

    5

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -14,6 +14,7 @@ namespace ConsoleApplication1
             Console.WriteLine(2);
             Console.WriteLine(3);
             Console.WriteLine(4);
+            Console.WriteLine(5);
         }
     }
 }
